aa59120fe9c26716d20c13ba50f9b4a4ec5f774e
Mathias Meiser <mathias.meiser@sap.com>
2017-08-02 12:58:05 +0000

    added steam passport for claim api

---
+++ api-claim/logic.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
+const SteamStrategy = require('passport-steam').Strategy;
+
+
+module.exports = function () {
+
+    // Simple route middleware to ensure user is authenticated.
+    //   Use this route middleware on any resource that needs to be protected.  If
+    //   the request is authenticated (typically via a persistent login session),
+    //   the request will proceed.  Otherwise, the user will be redirected to the
+    //   login page.
+    function ensureAuthenticated(req, res, next) {
+        if (req.isAuthenticated()) { return next(); }
+        res.redirect('/');
+    }
+
+    const app = express();
+
+
+    passport.serializeUser(function (user, done) {
+        done(null, user);
+    });
+
+    passport.deserializeUser(function (obj, done) {
+        done(null, obj);
+    });
+
+    passport.use(new SteamStrategy({
+        returnURL: 'http://192.168.157.131:3001/auth/return',
+        realm: 'http://192.168.157.131:3001/',
+        profile: false
+    },
+        function (identifier, profile, done) {
+            // asynchronous verification, for effect...
+            process.nextTick(function () {
+
+                // TODO:
+                // check if in the uploaded game the user participated under offer conditions
+                // if user does his first claim, register him with users mapping in contract
+                // send the payout if conditions are met
+
+                return done(null, identifier);
+            });
+        }
+    ));
+
+    app.use(session({
+        secret: 'your secret',
+        name: 'name of session id',
+        resave: true,
+        saveUninitialized: true
+    }));
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    app.get('/', function (req, res) {
+         res.status(200).send('claim api v0.1');
+    });
+
+    app.get('/account', ensureAuthenticated, function (req, res) {
+         res.status(200).send(req.user);
+    });
+
+    app.get('/logout', function (req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+    app.get('/auth',
+        passport.authenticate('steam', { failureRedirect: '/' }),
+        function (req, res) {
+            res.redirect('/');
+        });
+
+    app.get('/auth/return',
+        passport.authenticate('steam', { failureRedirect: '/' }),
+        function (req, res) {
+            res.redirect('/');
+        });
+
+    return app;
+};
