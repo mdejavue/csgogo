6d81c3dfad7e90552182a45d7c39734f0b6d958f
vagrant <mdejavue@github.com>
2017-08-02 09:48:46 +0000

    now parsing id

--- server/logic.js
+++ server/logic.js
@@ -11,8 +11,9 @@ module.exports = function () {
             let i = 0;
             let t = p.texts;
             return {
+                id : p.id,
                 name : t[i++],
-                team : t.length > 10 ? t[i++].substr(1) : "",
+                name2 : t.length > 10 ? t[i++].substr(1) : "",
                 kills : t[i++],
                 assists : t[i++],
                 deaths : t[i++],
@@ -37,17 +38,28 @@ module.exports = function () {
         let aPlayers = [];
 
         parser.on('startTag', (name, attrs) => {
-            if (name === "td") {
+            if (name === 'td') {
                 let isPlayer = attrs.find(attr => {
-                    return attr.name === "width" && attr.value === "300";
+                    return attr.name === 'width' && attr.value === '300';
                 });
 
                 if (isPlayer) {
                     aPlayers.push({
-                        texts : []
+                        texts : [],
+                        id : ""
                     })
                 }
             }
+
+            if (aPlayers.length > 0) {
+                let currPlayer = aPlayers[aPlayers.length -1];
+                if (name === 'a' && attrs.length > 0 && currPlayer.texts.length <= 11) {
+                    if (attrs[0].name === 'href' && attrs[0].value.startsWith('/player/')) {
+                        currPlayer.id = attrs[0].value.split("player/")[1].replace("~", "");
+                    }
+                }
+            }
+
         });
 
         parser.on('text', (text) => {
@@ -67,7 +79,7 @@ module.exports = function () {
 
         });
 
-        request.get('http://www.csgo-stats.net/match/' + matchId).pipe(parser);        
+        request.get('http://www.csgo-stats.net/match/' + matchId).pipe(parser);
     });
 
     return app;
