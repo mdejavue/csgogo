2658283411afbae2497cc28850bcab6fa1c719eb
Mathias Meiser <mathias.meiser@sap.com>
2017-08-09 12:01:36 +0000

    claim matchlinks

--- web/js/controller/Claim.controller.js
+++ web/js/controller/Claim.controller.js
@@ -4,6 +4,14 @@ sap.ui.define([
     "use strict";
 
     return BaseController.extend("de.javue.csgogo.controller.Claim", {
+        addMatchLink : function(oEvent) {
+            this.byId("matchLinkContainer").addContent(sap.ui.xmlfragment("de.javue.csgogo.fragment.MatchLink", this));
+        },
         
+        removeMatchLink : function(oEvent) {
+            if (this.byId("matchLinkContainer").getContent().length > 2) {
+                this.byId("matchLinkContainer").removeContent(oEvent.getSource().getParent());
+            }
+        }
     });
 });
